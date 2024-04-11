49361f127eb910eff4b236fbaad8bab301e5fccc
anpeni <105881030+anpeni@users.noreply.github.com>
2024-04-11 13:42:23 +0200

    Create v_06.ts

---
+++ v_06.ts
@@ -0,0 +1,8 @@
+app.post('/submit-credit-card', (req, res) => {
+    // Aquí, la información de la tarjeta de crédito se transmite sin cifrado.
+    const creditCardNumber = req.body.creditCardNumber;
+    // ...
+});
+
+//Exposición de Datos Sensibles:
+//El código no emplea cifrado al enviar o recibir datos sensibles, lo cual podría exponer la información del usuario durante una transmisión.

7bc85945f2e9356eb15a793405735c9edeb37810
anpeni <105881030+anpeni@users.noreply.github.com>
2024-04-11 13:44:17 +0200

    Create v_09.ts

---
+++ v_09.ts
@@ -0,0 +1,8 @@
+// La aplicación no establece encabezados de seguridad como Content-Security-Policy, X-Frame-Options, etc.
+app.use((req, res, next) => {
+    // No se configuran cabeceras de seguridad
+    next();
+});
+
+//Configuración de Seguridad Incorrecta:
+//Los headers de seguridad HTTP no están configurados o son inadecuados, dejando la aplicación vulnerable a varios ataques de navegador.
